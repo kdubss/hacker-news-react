9dac48d89215673fd4e27f6fa142dcc9a595f923
Kang Wang <kwang.inbox@gmail.com>
2019-11-25 22:08:21 -0800

    Add list of objects & map through them in return JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 function App() {
   return (
     <div className="App">
-      <h1>Hacker News React App</h1>
+      <ul style={{listStyle: 'none'}}>
+        {list.map(l => {
+          return (
+            <div>
+              <div>
+                <li>Title: {l.title}</li>
+                <li>URL: {l.url}</li>
+                <li>Author: {l.author}</li>
+                <li>No. Comments: {l.num_comments}</li>
+                <li>Points: {l.points}</li>
+                <li>ObjectID: {l.objectID}</li>
+              </div>
+              <hr />
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 }
