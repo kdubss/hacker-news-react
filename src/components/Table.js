3ba578214b3252c7f773a2f711b86ead1e05fb1a
Kang Wang <kwang.inbox@gmail.com>
2019-12-05 23:08:41 -0800

    Refactor Button and Table components into functional type components via. ES6 conventions

--- src/components/Table.js
+++ src/components/Table.js
@@ -8,28 +8,21 @@ const isSearched = searchTerm => {
   };
 };
 
-class Table extends React.Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-
-    return (
-      <div>
-        { list.filter(isSearched(pattern)).map(item =>
-          <div key={ item.objectID }>
-            <span>
-              <a href={ item.url }>{ item.title }</a>
-            </span>
-            <span>{ item.author }</span>
-            <span>{ item.num_comments }</span>
-            <span>{ item.points }</span>
-            <Button onClick={ () => onDismiss(item.objectID) }>
-              Dismiss
-            </Button>
-          </div>
-        )}
+const Table = ({ list, pattern, onDismiss }) =>
+  <div>
+    { list.filter(isSearched(pattern)).map(item =>
+      <div key={ item.objectID }>
+        <span>
+          <a href={ item.url }>{ item.title }</a>
+        </span>
+        <span>{ item.author }</span>
+        <span>{ item.num_comments }</span>
+        <span>{ item.points }</span>
+        <Button onClick={ () => onDismiss(item.objectID) }>
+          Dismiss
+        </Button>
       </div>
-    );
-  };
-};
+    )}
+  </div>
 
 export default Table;
