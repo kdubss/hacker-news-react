8373054a151b6828b2bd967845db270eb348b3d6
Kang Wang <kwang.inbox@gmail.com>
2019-12-05 22:53:21 -0800

    Refactor SearchBar component from claas-based to functional component via. ES6 (e.g. functional destructuring).

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,12 @@
 import React from 'react';
 
-class SearchBar extends React.Component {
-  render() {
-    const { value, onChange, children } = this.props;
-
-    return (
-      <div className="App">
-        <form>
-          { children } <input
-            type="text"
-            value={ value }
-            onChange={ onChange }
-          />
-        </form>
-      </div>
-    );
-  }
-}
+const SearchBar = ({ value, onChange, children }) =>
+  <form>
+    { children } <input
+      type="text"
+      value={ value }
+      onChange={ onChange }
+    />
+  </form>
 
 export default SearchBar;
