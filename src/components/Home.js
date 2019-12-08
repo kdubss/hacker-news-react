60d7fad0e0dc6e4567bd5c467292a3335f0cb9f5
Kang Wang <kwang.inbox@gmail.com>
2019-12-07 22:48:22 -0800

    Add API fetch query w/ native JS fetch API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import list from '../data/example-list';
 import SearchBar from './SearchBar';
@@ -6,16 +6,25 @@ import Table from './Table';
 
 import '../App.css';
 
+const DEFAULT_QUERY = 'react';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       list,
-      searchTerm: '',
+      searchTerm: DEFAULT_QUERY,
+      result: null,
     };
 
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
   };
 
   render() {
@@ -64,6 +73,20 @@ class Home extends React.Component {
   onSearchChange(e) {
     this.setState({ searchTerm: e.target.value });
   };
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  };
+
+  // Once component JSX is rendered on the client and visible to the user,
+  // 'componentDidMount' is called.
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+  }
 };
 
 export default Home;
