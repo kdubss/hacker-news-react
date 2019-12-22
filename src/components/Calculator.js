7e6e47fe5d49e8b2728a5a8776d4b1351ea04dc9
Kang Wang <kwang.inbox@gmail.com>
2019-12-22 16:02:57 -0500

    Life state up from each Temperature input component to the nearest parent component - Calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,32 +3,70 @@ import React from 'react';
 import BoilingVerdict from './BoilingVerdict';
 import TemperatureInput from './TemperatureInput';
 
+const fahrenheitToCelsius = fahrenheit => (fahrenheit - 32) * (5/9);
+const celsiusToFahrenheit = celsius => (celsius * (9/5)) + 32;
+
+const tryConvert = (temperature, converterFunc) => {
+  const inputTemperature = parseFloat(temperature);
+
+  if (Number.isNaN(inputTemperature)) {
+    return '';
+  };
+
+  const output = converterFunc(inputTemperature);
+  const roundedOutput = Math.round(output * 1000) / 1000;
+
+  return roundedOutput.toString();
+};
+
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       temperature: '',
+      scale: 'c',
     };
 
-    this.onChange = this.onChange.bind(this);
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+  };
+
+  handleCelsiusChange(temperature) {
+    console.log(temperature);
+    this.setState({ temperature: temperature, scale: 'c' });
+  };
+
+  handleFahrenheitChange(temperature) {
+    this.setState({ temperature: temperature, scale: 'f' });
+  }
+
+  onChange(event) {
+    this.setState({ temperature: event.target.value });
   };
 
   render() {
+    const scale = this.state.scale;
     const temperature = this.state.temperature;
+    const celsius = scale === 'f' ? tryConvert(temperature, fahrenheitToCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConvert(temperature, celsiusToFahrenheit) : temperature;
 
     return (
       <div>
-        <TemperatureInput scale="c" />
-        <TemperatureInput scale="f" />
+        <TemperatureInput
+          scale="c"
+          temperature={ celsius }
+          onTemperatureChange={ this.handleCelsiusChange }
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={ fahrenheit }
+          onTemperatureChange={ this.handleFahrenheitChange }
+        />
         <BoilingVerdict
           temperatureC={ parseFloat(this.state.temperature) }
         />
       </div>
     );
-  };
-
-  onChange(event) {
-    this.setState({ temperature: event.target.value });
   };
 };
 
--- src/components/TemperatureInput.js
+++ src/components/TemperatureInput.js
@@ -5,36 +5,19 @@ const scaleNames = {
   f: 'Fahrenheit',
 };
 
-const fahrenheitToCelsius = fahrenheit => (fahrenheit - 32) * (5/9);
-const celsiusToFahrenheit = celsius => (celsius * (9/5)) + 32;
-
-const tryConvert = (temperature, converterFunc) => {
-  const inputTemperature = parseFloat(temperature);
-
-  if (Number.isNaN(inputTemperature)) {
-    return '';
-  };
-
-  const output = converterFunc(inputTemperature);
-  const roundedOutput = Math.round(output * 1000) / 1000;
-
-  return roundedOutput.toString();
-}
-
 class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      temperature: '',
-    };
-
     this.handleChange = this.handleChange.bind(this);
   };
 
+  handleChange(event) {
+    this.props.onTemperatureChange(event.target.value);
+  };
+
   render() {
-    const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
     const scale = this.props.scale;
-    console.log('props: ', this.props);
 
     return (
       <div>
@@ -48,11 +31,6 @@ class TemperatureInput extends React.Component {
       </div>
     );
   };
-
-  handleChange(event) {
-    console.log(event);
-    this.setState({ temperature: event.target.value });
-  }
 };
 
 export default TemperatureInput;
