7426d138c664e02fcf0fa2a0d87ee54d7376124a
Kang Wang <kwang.inbox@gmail.com>
2019-12-27 22:28:05 -0500

    Implement snapshot testing to the Home component

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Home';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+describe('Home component snapshot test', () => {
 
-  ReactDOM.render(<Home />, div);
-  ReactDOM.unmountComponentAtNode(div);
-})
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<Home />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('Home component has a valid snapshot.', () => {
+    const component = renderer.create(
+        <Home />
+      );
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
