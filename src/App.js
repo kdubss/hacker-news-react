f1446635f28787ac5e2562590af09d7d9fb6c676
Kang Wang <kwang.inbox@gmail.com>
2020-01-15 22:38:43 -0800

    Render a list of items using the .map function to return JSX.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    auuthor: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends React.Component {
   render() {
-    const welcomeMessage = (
-      <h1>
-        Welcome to React!
-      </h1>
-    );
-    const welcomeBlurb = <p>Start your coding adventures, here!</p>
+    const listItems = list.map(item => {
+      return (
+        <div>
+          { item.title }
+        </div>
+      );
+    });
+    
     return (
       <div className="App">
-        <span>{ welcomeMessage }</span>
-        { welcomeBlurb }
+        <span>Here's a list of items!</span>
+        { listItems }
       </div>
     );
   };
