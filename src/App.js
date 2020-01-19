748d3d15205947ca59a7655fecb7e2016d0a0a97
Kang Wang <kwang.inbox@gmail.com>
2020-01-18 21:12:17 -0800

    Break out the Searchbar and Table - previously wrapped by App component - to their own separate components in App.

--- src/App.js
+++ src/App.js
@@ -48,39 +48,68 @@ class App extends React.Component {
 
   render() {
     const { searchTerm, list } = this.state;
-    const renderSearchbar = (
-      <form>
-        <input type="text" onChange={ this.onSearchChange } />
-      </form>
-    );
-
-    const renderItems = list.filter(isSearched(searchTerm)).map(item =>
-      <div key={ item.objectID }>
-        <span>
-          <a href={ item.url }>{ item.url }</a>
-        </span>
-        <span>{ item.author }</span>
-        <span>{ item.num_comments }</span>
-        <span>{ item.points }</span>
-        <span>
-          <button
-            onClick={ () => this.onDismiss(item.objectID) }
-            type="button"
-          >
-            Dismiss
-          </button>
-        </span>
-      </div>
-    );
 
     return (
       <div className="App">
-        <span>Here's a Searchibar and a list of items!</span>
-        { renderSearchbar }
-        { renderItems }
+				<Search
+					value={ searchTerm }
+					onChange={ this.onSearchChange }
+				/>
+				<Table
+					list={ list }
+					pattern={ searchTerm }
+					onDismiss={ this.onDismiss }
+				/>
       </div>
     );
   };
+};
+
+class Search extends React.Component {
+	render() {
+		const { value, onChange } = this.props;
+		
+		return (
+			<form>
+				<input
+					type="text"
+					value={ value }
+					onChange={ onChange }
+				/>
+			</form>		
+		);
+	};
+};
+
+class Table extends React.Component {
+	render() {
+    const { list, pattern, onDismiss } = this.props;
+
+		return (
+      <div>
+        {
+          list.filter(isSearched(pattern)).map(item =>
+            <div key={ item.objectID }>
+              <span>
+                <a href={ item.url }>{ item.url }</a>
+              </span>
+              <span>{ item.author }</span>
+              <span>{ item.num_comments }</span>
+              <span>{ item.points }</span>
+              <span>
+                <button
+                  onClick={ () => onDismiss(item.objectID) }
+                  type="button"
+                >
+                  Dismiss
+                </button>
+              </span>
+            </div>
+          )
+        }
+      </div>
+		);
+	};
 };
 
 export default App;
