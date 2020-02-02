63dd2d35319c3051418459901b4df3ad7a1555c5
Kang Wang <kwang.inbox@gmail.com>
2020-02-01 21:25:36 -0800

    Add errors to local state, catch errors on data fetch from api, and conditionally render data table depending on experience of api data-fetch errors

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      error: null,
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
@@ -31,7 +32,7 @@ class App extends React.Component {
   }
 
   setSearchTopStories(result) {
-    const { hits, page } = result;
+    const { hits } = result;
     const { searchKey, results } = this.state;
     const oldHits = results && results[searchKey]
       ? results[searchKey].hits
@@ -62,7 +63,7 @@ class App extends React.Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({ error }));
   }
 
   componentDidMount() {
@@ -81,7 +82,7 @@ class App extends React.Component {
    */
   onDismiss(id) {
     const { searchKey, results } = this.state;
-    const { hits, page } = results[searchKey]
+    const { hits } = results[searchKey]
 
     const isNotId = item => item.objectID !== id;
     const updatedHits = hits.filter(isNotId);
@@ -98,6 +99,7 @@ class App extends React.Component {
       searchTerm,
       results,
       searchKey,
+      error,
     } = this.state;
 
     const page = (
@@ -123,10 +125,15 @@ class App extends React.Component {
             Search
           </Search>
         </div>
-        <Table
-          list={ list }
-          onDismiss={ this.onDismiss }
-        />
+        { error
+          ? <div className="interactions">
+              <p>Something went wrong...</p>
+            </div>
+          : <Table
+              list={ list }
+              onDismiss={ this.onDismiss }
+            />
+        }
         <div className="interactions">
           <Button onClick={ () => this.fetchSearchTopStories(searchKey, page + 1) }>
             More Hits!
